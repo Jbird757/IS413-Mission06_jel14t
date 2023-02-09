e911ad403a3d365b6c4f730cb9d213a21900ab86
jel14t <jay.lowry350@gmail.com>
2023-02-08 19:40:03 -0700

    Add project files.

---
+++ IS413-Mission06_jel14t/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IS413_Mission06_jel14t.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
